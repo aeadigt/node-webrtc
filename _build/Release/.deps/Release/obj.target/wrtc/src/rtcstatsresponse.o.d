6bec2397cb45a6491ba03b628d44f3cf3cd1dbc9
aea <aea@digt.ru>
2017-08-04 17:01:50 +0300

    Дефолтный билд

---
+++ _build/Release/.deps/Release/obj.target/wrtc/src/rtcstatsresponse.o.d
@@ -0,0 +1,105 @@
+cmd_Release/obj.target/wrtc/src/rtcstatsresponse.o := g++ '-DNODE_GYP_MODULE_NAME=wrtc' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DWEBRTC_THREAD_RR' '-DEXPAT_RELATIVE_PATH' '-DGTEST_RELATIVE_PATH' '-DJSONCPP_RELATIVE_PATH' '-DWEBRTC_RELATIVE_PATH' '-D_GLIBCXX_USE_CXX11_ABI=0' '-DWEBRTC_LINUX' '-DWEBRTC_POSIX=1' '-DBUILDING_NODE_EXTENSION' -I/home/aea/.node-gyp/6.10.3/include/node -I/home/aea/.node-gyp/6.10.3/src -I/home/aea/.node-gyp/6.10.3/deps/uv/include -I/home/aea/.node-gyp/6.10.3/deps/v8/include -I../node_modules/nan -I../third_party/webrtc/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -pthread -fno-exceptions -fno-strict-aliasing -Wall -Wno-unused-parameter -Wno-missing-field-initializers -Wextra -Wno-unused-local-typedefs -Wno-uninitialized -Wno-unused-variable -Wno-unused-but-set-variable -pipe -fno-ident -fdata-sections -ffunction-sections -fPIC -fpermissive -std=c++11 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/wrtc/src/rtcstatsresponse.o.d.raw   -c -o Release/obj.target/wrtc/src/rtcstatsresponse.o ../src/rtcstatsresponse.cc
+Release/obj.target/wrtc/src/rtcstatsresponse.o: \
+ ../src/rtcstatsresponse.cc ../src/rtcstatsresponse.h \
+ ../node_modules/nan/nan.h \
+ /home/aea/.node-gyp/6.10.3/include/node/node_version.h \
+ /home/aea/.node-gyp/6.10.3/include/node/uv.h \
+ /home/aea/.node-gyp/6.10.3/include/node/uv-errno.h \
+ /home/aea/.node-gyp/6.10.3/include/node/uv-version.h \
+ /home/aea/.node-gyp/6.10.3/include/node/uv-unix.h \
+ /home/aea/.node-gyp/6.10.3/include/node/uv-threadpool.h \
+ /home/aea/.node-gyp/6.10.3/include/node/uv-linux.h \
+ /home/aea/.node-gyp/6.10.3/include/node/node.h \
+ /home/aea/.node-gyp/6.10.3/include/node/v8.h \
+ /home/aea/.node-gyp/6.10.3/include/node/v8-version.h \
+ /home/aea/.node-gyp/6.10.3/include/node/v8config.h \
+ /home/aea/.node-gyp/6.10.3/include/node/node_version.h \
+ /home/aea/.node-gyp/6.10.3/include/node/node_buffer.h \
+ /home/aea/.node-gyp/6.10.3/include/node/node.h \
+ /home/aea/.node-gyp/6.10.3/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h \
+ /home/aea/.node-gyp/6.10.3/include/node/v8.h \
+ ../third_party/webrtc/include/webrtc/api/statstypes.h \
+ ../third_party/webrtc/include/webrtc/base/basictypes.h \
+ ../third_party/webrtc/include/webrtc/base/common.h \
+ ../third_party/webrtc/include/webrtc/base/constructormagic.h \
+ ../third_party/webrtc/include/webrtc/base/linked_ptr.h \
+ ../third_party/webrtc/include/webrtc/base/refcount.h \
+ ../third_party/webrtc/include/webrtc/base/atomicops.h \
+ ../third_party/webrtc/include/webrtc/base/scoped_ptr.h \
+ ../third_party/webrtc/include/webrtc/base/deprecation.h \
+ ../third_party/webrtc/include/webrtc/base/template_util.h \
+ ../third_party/webrtc/include/webrtc/typedefs.h \
+ ../third_party/webrtc/include/webrtc/base/scoped_ref_ptr.h \
+ ../third_party/webrtc/include/webrtc/base/stringencode.h \
+ ../third_party/webrtc/include/webrtc/base/checks.h \
+ ../third_party/webrtc/include/webrtc/base/thread_checker.h \
+ ../third_party/webrtc/include/webrtc/base/thread_checker_impl.h \
+ ../third_party/webrtc/include/webrtc/base/criticalsection.h \
+ ../third_party/webrtc/include/webrtc/base/thread_annotations.h \
+ ../third_party/webrtc/include/webrtc/base/platform_thread_types.h \
+ ../src/common.h ../src/rtcstatsreport.h
+../src/rtcstatsresponse.cc:
+../src/rtcstatsresponse.h:
+../node_modules/nan/nan.h:
+/home/aea/.node-gyp/6.10.3/include/node/node_version.h:
+/home/aea/.node-gyp/6.10.3/include/node/uv.h:
+/home/aea/.node-gyp/6.10.3/include/node/uv-errno.h:
+/home/aea/.node-gyp/6.10.3/include/node/uv-version.h:
+/home/aea/.node-gyp/6.10.3/include/node/uv-unix.h:
+/home/aea/.node-gyp/6.10.3/include/node/uv-threadpool.h:
+/home/aea/.node-gyp/6.10.3/include/node/uv-linux.h:
+/home/aea/.node-gyp/6.10.3/include/node/node.h:
+/home/aea/.node-gyp/6.10.3/include/node/v8.h:
+/home/aea/.node-gyp/6.10.3/include/node/v8-version.h:
+/home/aea/.node-gyp/6.10.3/include/node/v8config.h:
+/home/aea/.node-gyp/6.10.3/include/node/node_version.h:
+/home/aea/.node-gyp/6.10.3/include/node/node_buffer.h:
+/home/aea/.node-gyp/6.10.3/include/node/node.h:
+/home/aea/.node-gyp/6.10.3/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+/home/aea/.node-gyp/6.10.3/include/node/v8.h:
+../third_party/webrtc/include/webrtc/api/statstypes.h:
+../third_party/webrtc/include/webrtc/base/basictypes.h:
+../third_party/webrtc/include/webrtc/base/common.h:
+../third_party/webrtc/include/webrtc/base/constructormagic.h:
+../third_party/webrtc/include/webrtc/base/linked_ptr.h:
+../third_party/webrtc/include/webrtc/base/refcount.h:
+../third_party/webrtc/include/webrtc/base/atomicops.h:
+../third_party/webrtc/include/webrtc/base/scoped_ptr.h:
+../third_party/webrtc/include/webrtc/base/deprecation.h:
+../third_party/webrtc/include/webrtc/base/template_util.h:
+../third_party/webrtc/include/webrtc/typedefs.h:
+../third_party/webrtc/include/webrtc/base/scoped_ref_ptr.h:
+../third_party/webrtc/include/webrtc/base/stringencode.h:
+../third_party/webrtc/include/webrtc/base/checks.h:
+../third_party/webrtc/include/webrtc/base/thread_checker.h:
+../third_party/webrtc/include/webrtc/base/thread_checker_impl.h:
+../third_party/webrtc/include/webrtc/base/criticalsection.h:
+../third_party/webrtc/include/webrtc/base/thread_annotations.h:
+../third_party/webrtc/include/webrtc/base/platform_thread_types.h:
+../src/common.h:
+../src/rtcstatsreport.h:
---
+++ _build/Release/.deps/Release/wrtc.node.d
@@ -0,0 +1,1 @@
+cmd_Release/wrtc.node := rm -rf "Release/wrtc.node" && cp -af "Release/obj.target/wrtc.node" "Release/wrtc.node"
